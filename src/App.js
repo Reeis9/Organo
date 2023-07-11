daeccd61fd31ecce022cee10ac691a59dc9a24de
Reeis9 <victorsi775@gmail.com>
2023-07-11 20:15:48 -0300

    Projeto React.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Banner from './componentes/Banner/Banner';
+import CampoTexto from './componentes/CampoTexto/Index';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Bem-vindo ao React
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Banner/>
+      <CampoTexto label="Nome"/>
+      <CampoTexto label="Cargo"/>
+      <CampoTexto label="Imagem"/>
     </div>
   );
 }
---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner(){
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt= "O banner principal do Organo"/>
+        </header>
+
+        
+    )
+}
+export default Banner
